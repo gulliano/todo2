82429a674e88a6765bd4aef3da080816f765f61e
gulliano <ggulliano@msn.com>
2023-10-09 12:17:32 -0400

    init

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import { View, Text } from 'react-native'
+import React, { useEffect } from 'react'
+import firestore from '@react-native-firebase/firestore'
+
+const App = () => {
+
+  useEffect(() => {
+    console.log("app start")
+    firestore().collection('Todo').get().then(snapShot=>{
+
+
+      console.log('snapShot' , snapShot)
+    })
+
+  }, [])
+
+
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
